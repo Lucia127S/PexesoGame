b2d0997778d8747da87d7dfba3cbcbc8e236b834
Lucia Stašová <stasova127lucia@gmail.com>
2023-01-25 19:32:24 +0100

    1.commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace PexesoGame
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// Simple game using List, conditions, loop, mouse event
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -26,12 +27,13 @@ namespace PexesoGame
 
         DispatcherTimer timer = new DispatcherTimer();
         int tenthsOfSecondElpased; // to keep track of the time elapsed
-        int MatchesFound; // number of matches the player has found
+        int MatchesFound; // number of matches the player has found in game
 
         public MainWindow()
         {
             InitializeComponent();
 
+            //setting timer
             timer.Interval = TimeSpan.FromSeconds(.1);
             timer.Tick += Timer_Tick;
 
@@ -51,6 +53,7 @@ namespace PexesoGame
 
         private void SetUpGame()
         {
+            //setting a list with emoji
             List<string> animalEmoji = new List<string>()
             {
                 "🐶","🐶",
@@ -62,16 +65,20 @@ namespace PexesoGame
                 "🦊","🦊",
                 "🦝","🦝",
             };
+
+            //random number generator
             Random random= new Random();
 
+            //setting visual of TextBoxes for game
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
                 if (textBlock.Name != "timeTextBlock")
                 {
-                    int index = random.Next(animalEmoji.Count);
-                    string nextEmoji = animalEmoji[index];
-                    textBlock.Text = nextEmoji;
-                    animalEmoji.RemoveAt(index);
+                    int index = random.Next(animalEmoji.Count); //index - number between 0 and the number of emoji left in the list
+                    string nextEmoji = animalEmoji[index];      // using random number to get a random emoji from list
+                    textBlock.Text = nextEmoji;                 //updating the text with random emoji
+                    animalEmoji.RemoveAt(index);                //remove used emoji 
+                    textBlock.Visibility= Visibility.Visible;
                 }
             }
 
@@ -82,11 +89,12 @@ namespace PexesoGame
 
         private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            //steps for not/finding pair
             TextBlock textBlock = sender as TextBlock;
             if(findinfMatch == false)
             {
                 textBlock.Visibility = Visibility.Hidden;
-                lastTextBlockClicked= textBlock;
+                lastTextBlockClicked= textBlock; // keeps track of TextBlock in case it needs to make it visible again
                 findinfMatch= true;
             }
             else if(textBlock.Text == lastTextBlockClicked.Text) 
@@ -95,7 +103,7 @@ namespace PexesoGame
                 textBlock.Visibility = Visibility.Hidden;
                 findinfMatch = false;
             }
-            else
+            else // not found - reset visibility
             {
                 lastTextBlockClicked.Visibility = Visibility.Visible;   
                 findinfMatch = false;
